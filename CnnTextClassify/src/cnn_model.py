c5d2208b10c6d86ccceaad8394154a6233783cf2
masterdai <masterdai@126.com>
2019-05-25 13:58:03 +0800

    update the file

--- CnnTextClassify/src/cnn_model.py
+++ CnnTextClassify/src/cnn_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from data.dataset import *
 
 
 class CnnModel(object):
@@ -13,14 +14,16 @@ class CnnModel(object):
         else:
             self.vocab_size = vocab_size
             self.word_dim = 128
-            self.embedding = tf.get_variable("embedding", shape=[self.vocab_size, self.word_dim],
-                                             initializer=tf.random_normal_initializer(mean=0, stddev=0.02))
+            self.embedding = tf.Variable(tf.random_normal(shape=(self.vocab_size, self.word_dim), mean=0, stddev=0.1),
+                                         dtype=tf.float32,
+                                         trainable=True)
 
         self.cnn_kernel = [5, self.word_dim, 1, 3]  # 目的在于保证在word_dim方向不移动
         self.pool_kernel = [1, 5, 1, 1]
         self.input = tf.placeholder(shape=(self.batch, self.max_len), dtype=tf.int32)
         self.label_num = 10
-        self.label = tf.placeholder(shape=(self.batch, self.label_num))
+        self.label = tf.placeholder(shape=(self.batch, self.label_num), dtype=tf.int32)
+        self.session = tf.Session()
         pass
 
     def embedding_layer(self, input, name="embedding"):
@@ -37,7 +40,7 @@ class CnnModel(object):
         # return : [batch,out_heigth,out_with,kernel_number]
         with tf.name_scope(name=name):
             cnn_input = tf.expand_dims(input, axis=-1)
-            return tf.nn.conv2d(cnn_input, kernel, [1, 1, 1, 1], name=name)
+            return tf.nn.conv2d(cnn_input, kernel, [1, 1, 1, 1], padding="VALID", name=name)
 
     def maxpool_layer(self, input, pool_kernel, name="max_pool"):
         with tf.name_scope(name=name):
@@ -45,24 +48,31 @@ class CnnModel(object):
 
     def dense_lyaer(self, input, hidden_dim, name="dense"):
         with tf.name_scope(name=name):
-            tf.layers.dense(input, hidden_dim, name=name)
-        pass
+            return tf.layers.dense(input, hidden_dim, name=name)
 
     def build_model(self):
 
         embedding = self.embedding_layer(self.input)
         embedding_dropout = self.dropout_layer(embedding)
-        cnn_out = self.cnn_layer(embedding_dropout, self.cnn_kernel)
+        kernel = tf.Variable(tf.random_normal(shape=self.cnn_kernel, mean=0, stddev=0.1),
+                             dtype=tf.float32)
+        cnn_out = self.cnn_layer(embedding_dropout, kernel)
         max_out = self.maxpool_layer(cnn_out, self.pool_kernel)  # [batch, max_heigth,1,max_number]
         max_out = tf.squeeze(max_out, axis=-2)  ## [batch, max_heigth,max_number]
-        self.logits = self.dense_lyaer(max_out.reshape(self.batch, None), self.label_num)
-        self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)
-        loss = tf.nn.softmax_cross_entropy_with_logits(self.logits, self.label)
-        self.loss = tf.reduce_sum(loss)
-        opt = tf.train.AdamOptimizer(loss)
-        return opt, self.loss, self.y_pred_cls
+        input = tf.reshape(max_out, shape=[self.batch, -1])
+        self.logits = self.dense_lyaer(input, self.label_num)
+        soft_max = tf.nn.softmax(self.logits)
+        y_pred_cls = tf.argmax(soft_max, 1)
+        loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.label, logits=self.logits)
+        correct_pred = tf.equal(tf.argmax(self.label, 1), y_pred_cls)
+        loss = tf.reduce_sum(loss)
+
+        opt = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss=loss)
+
+        acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        return opt, loss, y_pred_cls, acc
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     model = CnnModel(None, vocab_size=5)
     model.build_model()
--- CnnTextClassify/src/train.py
+++ CnnTextClassify/src/train.py
@@ -3,12 +3,33 @@ import tensorflow as tf
 
 base_dir = os.path.dirname(__file__)
 sys.path.insert(0, base_dir)
-from cnn_model import CnnModel
+from data.dataset import *
+from cnn_model import *
 
-if __name__ == "main":
-    model = CnnModel()
-    session = tf.Session()
-    init = tf.global_variables_initializer()
-    session.run(init)
-    opt, loss, pred = model.build_model()
-    print('hello')
+batch_size = 64
+train_file = "../data/cnews.train.txt"
+vocab_file = "../data/cnews.vocab.txt"
+words, word2id = read_vocab(vocab_file)
+vocab_size = 5000
+category, cat2id = read_category()
+max_seq_len = 600
+x_pad, y_pad = process_file(train_file, word2id, cat2id, max_seq_len)
+model = CnnModel(None, vocab_size, batch=batch_size, sequence_max_len=max_seq_len)
+session = model.session
+opt, loss, pred, acc = model.build_model()
+init = tf.global_variables_initializer()
+session.run(tf.local_variables_initializer())
+session.run(tf.initialize_all_variables())
+session.run(init)
+
+for epoch in range(4):
+
+    print("epoch ", epoch)
+    i = 0
+    for input_batch, label_batch in batch_iter(x_pad, y_pad, batch_size):
+        # print(len(input_batch, label_batch)
+        _, batch_loss, batch_pred, batch_acc = session.run([opt, loss, pred, acc],
+                                                           feed_dict={model.input: input_batch,
+                                                                      model.label: label_batch})
+        print("step={},loss={},epoch={},acc={}".format(i, batch_loss, epoch, batch_acc))
+        i = i + 1
